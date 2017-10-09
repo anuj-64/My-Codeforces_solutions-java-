f3b1d7456ce1616c3060d60660bca5970aad8eec
Anuj Singh <Anuj.s.csit14@hotmail.com>
2017-10-09 11:13:03 +0530

    Add files via upload

---
+++ 59-A/59-A-16062759.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Solution{
+		public static void main(String args[]){
+			Scanner in = new Scanner(System.in);
+		    String str=in.next();
+		    String str1=str.toUpperCase();
+		    String str2=str.toLowerCase();
+		    int u=0,l=0;
+		    for(int i=0;i<str.length();i++){
+		    	if(str.charAt(i)!=str1.charAt(i))
+		    		u++;
+		    	if(str.charAt(i)!=str2.charAt(i))l++;
+		    }
+			if(u<l)
+				System.out.println(str1);
+			else
+				System.out.println(str2);
+		}}
